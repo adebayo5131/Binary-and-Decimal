37336baf140a892adfeedf1aa6e49bf1c8c368fa
adebayo ajagunna <adebayoajagunna@yahoo.com>
2018-06-12 14:29:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+
+
+
+
+function myFunction() {
+
+     rotate();
+
+   /* console.log("1 "+document.getElementById("circleByte"));//print*/
+    var x = document.getElementById("digitAge").value;
+  
+    if(x.length==0){
+        window.alert("Enter a digit");
+        //w
+       
+    }
+     else{
+        var num = parseInt(x,10);
+        var answer = (num).toString(2);
+        document.getElementById("demo").innerHTML = "your age in binary";
+        document.getElementById("showBinary").innerHTML = answer;     
+    }
+    
+    
+
+    
+   
+}
+
+
+
+
+
+function myBinary() {
+
+ 
+
+    rotate();
+
+    /* console.log("1 "+document.getElementById("circleByte"));//print*/
+    var x = document.getElementById("digitAge").value;
+
+    if(x.length==0){
+        window.alert("Enter a Binary");
+        //w
+
+    }
+    else{
+        var num = parseInt(x,2);
+        var answer = (num).toString(10);
+        
+        document.getElementById("demo").innerHTML = "your age in binary";
+        document.getElementById("showBinary").innerHTML = answer;     
+    }
+
+
+
+}
+
+function rotate()
+{
+
+    document.getElementById("circleByte").id="circleByteRotate";//rotate
+
+
+
+}
+
+
+
+
